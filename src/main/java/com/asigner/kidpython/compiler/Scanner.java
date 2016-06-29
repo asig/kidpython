544fdb933fe4e732eb6bc9c038cdec09e8de4c26
Andreas <andreas@fractal-labs.com>
2016-06-29 12:04:54 +0200

    scanner done

--- src/main/java/com/asigner/kidpython/compiler/Scanner.java
+++ src/main/java/com/asigner/kidpython/compiler/Scanner.java
@@ -1,8 +1,29 @@
 package com.asigner.kidpython.compiler;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 public class Scanner {
+
+    private static final Map<String, Token.Type> keywords = ImmutableMap.<String, Token.Type>builder()
+            .put("func", Token.Type.FUNC)
+            .put("for", Token.Type.FOR)
+            .put("end", Token.Type.END)
+            .put("if", Token.Type.IF)
+            .put("elseif", Token.Type.ELSEIF)
+            .put("else", Token.Type.ELSE)
+            .put("step", Token.Type.STEP)
+            .put("in", Token.Type.IN)
+            .put("do", Token.Type.DO)
+            .put("while", Token.Type.WHILE)
+            .put("repeat", Token.Type.REPEAT)
+            .put("until", Token.Type.UNTIL)
+            .put("return", Token.Type.RETURN)
+            .put("and", Token.Type.AND)
+            .put("or", Token.Type.OR)
+            .build();
 
     private final String text;
 
@@ -15,6 +36,91 @@ public class Scanner {
         this.curPos = 0;
         this.line = 1;
         this.col = 0;
+    }
+
+    Token next() {
+        // Read over whitespace
+        while (Character.isWhitespace(getch())) ;
+
+        ungetch();
+
+        char c = getch();
+        int col = this.getCol();
+        int line = this.getLine();
+        if (Character.isLetter(c)) {
+            // Ident or keyword
+            StringBuffer b = new StringBuffer();
+            while(Character.isLetterOrDigit(c)) {
+                b.append(c);
+                c = getch();
+            }
+            ungetch();
+            String val = b.toString();
+            Token.Type t = keywords.get(val);
+            return new Token(t == null ? Token.Type.IDENT : t, line, col, val);
+        } else if (Character.isDigit(c)) {
+            StringBuffer b = new StringBuffer();
+            while(Character.isDigit(c)) {
+                b.append(c);
+                c = getch();
+            }
+            if (c == '.') {
+                b.append(c);
+                c = getch();
+                while(Character.isDigit(c)) {
+                    b.append(c);
+                    c = getch();
+                }
+            }
+            ungetch();
+            return new Token(Token.Type.NUM_LIT, line, col, b.toString());
+        } else {
+            switch(c) {
+                case '"':
+                case '\'': {
+                    StringBuffer b = new StringBuffer();
+                    char terminator = c;
+                    c = getch();
+                    while (c != terminator && c != 0) {
+                        b.append(c);
+                        c = getch();
+                    }
+                    return new Token(Token.Type.STRING_LIT, line, col, b.toString());
+                }
+                case '(': return new Token(Token.Type.LPAREN, line, col);
+                case ')': return new Token(Token.Type.RPAREN, line, col);
+                case '[': return new Token(Token.Type.LBRACK, line, col);
+                case ']': return new Token(Token.Type.RBRACK, line, col);
+                case '+': return new Token(Token.Type.PLUS, line, col);
+                case '-': return new Token(Token.Type.MINUS, line, col);
+                case '*': return new Token(Token.Type.ASTERISK, line, col);
+                case '/': return new Token(Token.Type.SLASH, line, col);
+                case '<': {
+                    char c2 = getch();
+                    if (c2 == '=') {
+                        return new Token(Token.Type.LE, line, col);
+                    } else if (c2 == '>') {
+                        return new Token(Token.Type.NE, line, col);
+                    } else {
+                        ungetch();
+                        return new Token(Token.Type.LT, line, col);
+                    }
+                }
+                case '>': {
+                    char c2 = getch();
+                    if (c2 == '=') {
+                        return new Token(Token.Type.GE, line, col);
+                    } else {
+                        ungetch();
+                        return new Token(Token.Type.GT, line, col);
+                    }
+                }
+                case '=': return new Token(Token.Type.EQ, line, col);
+                case ',': return new Token(Token.Type.COMMA, line, col);
+                case 0: return new Token(Token.Type.EOT, line, col);
+            }
+            return new Token(Token.Type.UNKNOWN, line, col, String.valueOf(c));
+        }
     }
 
     @VisibleForTesting
--- src/test/java/com/asigner/kidpython/compiler/ScannerTest.java
+++ src/test/java/com/asigner/kidpython/compiler/ScannerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ScannerTest {
+
     @Test
     public void testPositions() throws Exception {
         Scanner scanner = new Scanner("FOO\nBAR\n");
@@ -42,4 +43,49 @@ public class ScannerTest {
         c = scanner.getch(); assertEquals('F', c); assertEquals(1, scanner.getCol()); assertEquals(1, scanner.getLine());
     }
 
+    @Test
+    public void testToken() throws Exception {
+        Scanner scanner = new Scanner("func for end if elseif else step in do while repeat until return and or ()[]+-*/= <> < <= > >= , 1 12. 12.34 abc a2b \"foo\" 'bar'");
+        Token t;
+        t = scanner.next(); assertEquals(Token.Type.FUNC, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.FOR, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.END, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.IF, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.ELSEIF, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.ELSE, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.STEP, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.IN, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.DO, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.WHILE, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.REPEAT, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.UNTIL, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.RETURN, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.AND, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.OR, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.LPAREN, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.RPAREN, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.LBRACK, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.RBRACK, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.PLUS, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.MINUS, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.ASTERISK, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.SLASH, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.EQ, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.NE, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.LT, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.LE, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.GT, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.GE, t.getType());
+        t = scanner.next(); assertEquals(Token.Type.COMMA, t.getType());
+
+        t = scanner.next(); assertEquals(Token.Type.NUM_LIT, t.getType());  assertEquals("1", t.getValue());
+        t = scanner.next(); assertEquals(Token.Type.NUM_LIT, t.getType());  assertEquals("12.", t.getValue());
+        t = scanner.next(); assertEquals(Token.Type.NUM_LIT, t.getType());  assertEquals("12.34", t.getValue());
+
+        t = scanner.next(); assertEquals(Token.Type.IDENT, t.getType()); assertEquals("abc", t.getValue());
+        t = scanner.next(); assertEquals(Token.Type.IDENT, t.getType()); assertEquals("a2b", t.getValue());
+
+        t = scanner.next(); assertEquals(Token.Type.STRING_LIT, t.getType()); assertEquals("foo", t.getValue());
+        t = scanner.next(); assertEquals(Token.Type.STRING_LIT, t.getType()); assertEquals("bar", t.getValue());
+    }
 }
