7f17b32cfc3c74e100e3e907c082d7c984a9a4e0
Andreas <andreas@fractal-labs.com>
2016-07-07 14:33:24 +0200

    Escape codes for simpler usage

---
+++ src/main/java/com/asigner/kidpython/ide/util/AnsiEscapeCodes.java
@@ -0,0 +1,28 @@
+package com.asigner.kidpython.ide.util;
+
+public class AnsiEscapeCodes {
+    public final static String RESET = "\u001b[0m";
+    public final static String BOLD = "\u001b[1m";
+    public final static String ITALIC = "\u001b[2m";
+
+    public final static String IMAGE_NEGATIVE = "\u001b[7m";
+    public final static String IMAGE_POSITIVE = "\u001b[27m";
+
+    public final static String FG_BLACK   = "\u001b[30m";
+    public final static String FG_RED     = "\u001b[31m";
+    public final static String FG_GREEN   = "\u001b[32m";
+    public final static String FG_YELLOW  = "\u001b[33m";
+    public final static String FG_BLUE    = "\u001b[34m";
+    public final static String FG_MAGENTA = "\u001b[35m";
+    public final static String FG_CYAN    = "\u001b[36m";
+    public final static String FG_WHITE   = "\u001b[37m";
+
+    public final static String BG_BLACK   = "\u001b[40m";
+    public final static String BG_RED     = "\u001b[41m";
+    public final static String BG_GREEN   = "\u001b[42m";
+    public final static String BG_YELLOW  = "\u001b[43m";
+    public final static String BG_BLUE    = "\u001b[44m";
+    public final static String BG_MAGENTA = "\u001b[45m";
+    public final static String BG_CYAN    = "\u001b[46m";
+    public final static String BG_WHITE   = "\u001b[47m";
+}
