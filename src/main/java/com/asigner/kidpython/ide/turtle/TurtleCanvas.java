1ff0387c65f8d9b6501645527bf99ef830b6d8a7
Andreas Signer <asigner@gmail.com>
2016-07-12 00:12:47 +0200

    zoom, drag, clear

--- src/main/java/com/asigner/kidpython/ide/turtle/TurtleCanvas.java
+++ src/main/java/com/asigner/kidpython/ide/turtle/TurtleCanvas.java
@@ -5,7 +5,14 @@ package com.asigner.kidpython.ide.turtle;
 import com.asigner.kidpython.ide.util.SWTResources;
 import com.google.common.collect.Lists;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.MouseTrackListener;
+import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
@@ -21,17 +28,25 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.jfree.swt.SWTGraphics2D;
+import org.jfree.swt.SWTUtils;
 
 import java.awt.geom.AffineTransform;
 import java.util.List;
 
-public class TurtleCanvas extends Canvas {
+public class TurtleCanvas extends Canvas implements MouseListener, MouseMoveListener {
+
+    private static final int BTN_SIZE = 30;
+    private static final int BTN_BORDER = 10;
+
+    private static final float ZOOM_MIN = 0.2f;
+    private static final float ZOOM_MAX = 3.0f;
 
     private static final String ELUSIVE_REMOVE_CIRCLE = "\uf1db";
     private static final String ELUSIVE_DOWNLOAD = "\uf146";
     private static final String ELUSIVE_DOWNLOAD_ALT = "\uf145";
     private static final String ELUSIVE_ZOOM_IN = "\uf22f";
     private static final String ELUSIVE_ZOOM_OUT = "\uf230";
+    private static final String ELUSIVE_SCREENSHOT = "\uf1ec";
 
     public static class Line {
         private final Color color;
@@ -47,85 +62,140 @@ public class TurtleCanvas extends Canvas {
         }
     }
 
-    private final List<Line> lines;
+    private final List<Line> lines = Lists.newArrayList();
     private double angle;
     private double posX, posY;
     private boolean penDown;
     private boolean turtleVisible;
     private Color penColor;
     private int penWidth;
+    private float zoom;
+    private int offsetX, offsetY;
+
+    private boolean lmbDown = false;
+    private int lastMouseX;
+    private int lastMouseY;
+
+    private final Font elusiveFont;
 
     private List<Button> buttons = Lists.newArrayList();
 
     public TurtleCanvas(Composite parent, int style) {
         super(parent, style);
 
-        Font elusiveFont = new Font(this.getDisplay(), "elusiveicons", 10, SWT.NORMAL);
+        elusiveFont = new Font(this.getDisplay(), "elusiveicons", BTN_SIZE/3, SWT.NORMAL);
 
+        addButton(ELUSIVE_REMOVE_CIRCLE, this::reset);
+        addButton(ELUSIVE_DOWNLOAD_ALT, this::download);
+        addButton(ELUSIVE_ZOOM_IN, this::zoomIn);
+        addButton(ELUSIVE_ZOOM_OUT, this::zoomOut);
+        addButton(ELUSIVE_SCREENSHOT, this::center);
 
-        {
-            Button button = new Button(this, SWT.PUSH | SWT.FLAT | SWT.NO_TRIM);
-            button.setFont(elusiveFont);
-            button.setText(ELUSIVE_REMOVE_CIRCLE);
-            button.setSize(30, 30);
-            button.setLocation(10 + 0 * 30, 10);
-            button.setBackground(this.getDisplay().getSystemColor(SWT.COLOR_TRANSPARENT));
-            buttons.add(button);
-        }
-
-        {
-            Button button = new Button(this, SWT.PUSH | SWT.FLAT | SWT.NO_TRIM);
-            button.setFont(elusiveFont);
-            button.setText(ELUSIVE_DOWNLOAD_ALT);
-            button.setSize(30, 30);
-            button.setLocation(10 + 1 * 30, 10);
-            button.setBackground(this.getDisplay().getSystemColor(SWT.COLOR_TRANSPARENT));
-            buttons.add(button);
-        }
-
-        {
-            Button button = new Button(this, SWT.PUSH | SWT.FLAT | SWT.NO_TRIM);
-            button.setFont(elusiveFont);
-            button.setText(ELUSIVE_ZOOM_IN);
-            button.setSize(30, 30);
-            button.setLocation(10 + 2 * 30, 10);
-            button.setBackground(this.getDisplay().getSystemColor(SWT.COLOR_TRANSPARENT));
-            buttons.add(button);
-        }
-
-        {
-            Button button = new Button(this, SWT.PUSH | SWT.FLAT | SWT.NO_TRIM);
-            button.setFont(elusiveFont);
-            button.setText(ELUSIVE_ZOOM_OUT);
-            button.setSize(30, 30);
-            button.setLocation(10 + 3 * 30, 10);
-            button.setBackground(this.getDisplay().getSystemColor(SWT.COLOR_TRANSPARENT));
-            buttons.add(button);
-        }
-
-        this.lines = Lists.newArrayList();
-        this.angle = 0;
-        this.posX = 0;
-        this.posY = 0;
-        this.penDown = true;
-        this.turtleVisible = true;
-
-        penColor = SWTResources.getColor(new RGB(0,0,0));
-        penWidth = 1;
+        reset();
 
         addPaintListener(this::draw);
-        addListener (SWT.Resize,  new Listener() {
-            public void handleEvent (Event e) {
-                Rectangle rect = getClientArea ();
-                int x = rect.x + rect.width - buttons.size()*30 - 10;
-                for (Button b : buttons) {
-                    b.setLocation(x, rect.y + 10);
-                    x += 30;
-                }
+        addListener (SWT.Resize, this::onResize);
+        addMouseWheelListener(mouseEvent -> {
+            if (mouseEvent.count > 0) {
+                zoomIn();
+            } else {
+                zoomOut();
             }
         });
+        addMouseListener(this);
+        addMouseMoveListener(this);
     }
 
+    @Override
+    public void mouseDoubleClick(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void mouseDown(MouseEvent mouseEvent) {
+        if (mouseEvent.button == 1) {
+            lmbDown = true;
+            lastMouseX = mouseEvent.x;
+            lastMouseY = mouseEvent.y;
+        }
+    }
+
+    @Override
+    public void mouseUp(MouseEvent mouseEvent) {
+        if (mouseEvent.button == 1) {
+            lmbDown = false;
+        }
+    }
+
+    @Override
+    public void mouseMove(MouseEvent mouseEvent) {
+        if (lmbDown) {
+            int dx = lastMouseX - mouseEvent.x;
+            int dy = lastMouseY - mouseEvent.y;
+            lastMouseX = mouseEvent.x;
+            lastMouseY = mouseEvent.y;
+            offsetX += dx;
+            offsetY += dy;
+            Display.getCurrent().asyncExec(this::redraw);
+        }
+    }
+
+    private void center() {
+        offsetX = offsetY = 0;
+        Display.getCurrent().asyncExec(this::redraw);
+    }
+
+    private void onResize(Event e) {
+        Rectangle rect = getClientArea ();
+        int x = rect.x + rect.width - buttons.size()*BTN_SIZE - BTN_BORDER;
+        for (Button b : buttons) {
+            b.setLocation(x, rect.y + BTN_BORDER);
+            x += BTN_SIZE;
+        }
+    }
+
+    public void reset() {
+        lines.clear();
+        posX = posY = angle = 0;
+        penDown = true;
+        penColor = SWTResources.getColor(new RGB(0,0,0));
+        penWidth = 1;
+        turtleVisible = true;
+        zoom = 1;
+        offsetX = offsetY = 0;
+        Display.getCurrent().asyncExec(this::redraw);
+    }
+
+    public void download() {
+    }
+
+    public void zoomIn() {
+        if (zoom < ZOOM_MAX) {
+            zoom = zoom + .1f;
+            Display.getCurrent().asyncExec(this::redraw);
+        }
+    }
+
+    public void zoomOut() {
+        if (zoom > ZOOM_MIN) {
+            zoom = zoom - .1f;
+            Display.getCurrent().asyncExec(this::redraw);
+        }
+    }
+
+    private void addButton(String label, Runnable handler) {
+        Button button = new Button(this, SWT.PUSH | SWT.FLAT | SWT.CENTER);
+        button.setFont(elusiveFont);
+        button.setText(label);
+        button.setSize(BTN_SIZE, BTN_SIZE);
+        button.setBackground(this.getDisplay().getSystemColor(SWT.COLOR_TRANSPARENT));
+        button.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                handler.run();
+            }
+        });
+        buttons.add(button);
+    }
 
     public void move(double len) {
         if (len <= 0) {
@@ -179,7 +249,8 @@ public class TurtleCanvas extends Canvas {
         gc.fillRectangle(rect);
 
         Transform t = new Transform(gc.getDevice());
-        t.translate((rect.x + rect.width)/2.0f, (rect.y + rect.height)/2.0f);
+        t.translate((rect.x + rect.width)/2.0f - offsetX, (rect.y + rect.height)/2.0f - offsetY);
+        t.scale(zoom, zoom);
         gc.setTransform(t);
 
         gc.setLineCap(SWT.CAP_ROUND);
