4d078f731e8c63d7951a966968d3ae595dc2711f
asig <asigner@gmail.com>
2016-07-18 19:32:15 +0200

    Expect position as well

--- src/main/java/com/asigner/kidpython/compiler/runtime/ExecutionException.java
+++ src/main/java/com/asigner/kidpython/compiler/runtime/ExecutionException.java
@@ -1,7 +1,29 @@
 package com.asigner.kidpython.compiler.runtime;
 
+import com.asigner.kidpython.compiler.Position;
+
 public class ExecutionException extends Error {
+    private final Position pos;
+    private final String message;
+
     public ExecutionException(String message) {
-        super(message);
+        super();
+        this.pos = new Position(0,0);
+        this.message = message;
+    }
+
+    public ExecutionException(Position pos, String message) {
+        super();
+        this.pos = pos;
+        this.message = String.format("On line %d, column %d: %s", pos.getLine(), pos.getCol(), message);
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
